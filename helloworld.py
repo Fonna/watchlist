dfe232f5e5708402a480aaf48dac3fb9c6f8b927
Fonna <jiangpeng274@outlook.com>
2021-08-07 23:35:14 +0800

    添加helloworld

---
+++ helloworld.py
@@ -0,0 +1,18 @@
+# 导入flask
+from flask import Flask
+
+# 首先创建app对象
+# 全局统一管理路由、配置信息和视图信息
+app = Flask(__name__)
+
+
+# 有了app这个抽象的对象后，就可以定义路由和视图
+# 装饰器的作用是将路由器映射到视图函数index
+# 此处是根目录
+@app.route("/")
+def index():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run()
